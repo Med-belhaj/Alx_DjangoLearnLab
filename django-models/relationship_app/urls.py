edfc817a51be9b524b6bd74063676decee6c1156
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-08-25 21:55:35 +0100

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('library/<int:pk>/', views.library_detail, name='library_detail'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.user_register, name='register'),
 ]
