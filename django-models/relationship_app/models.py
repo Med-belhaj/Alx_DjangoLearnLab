3b994524d7357e2ad1ffa6f48b574a1118fffb13
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-08-25 22:06:01 +0100

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -58,3 +58,22 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
