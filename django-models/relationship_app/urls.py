acf9769723faf0b68cab2a4884d81528a5001f94
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-08-25 22:12:43 +0100

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view')
-    path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/', views.list_books, name='list_books'),  # List all books
+    path('books/add/', views.add_book, name='add_book'),  # URL pattern for adding a book
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),  # URL pattern for editing a book
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),  # URL pattern for deleting a book
 ]
