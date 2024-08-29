6f3bdff2b990b425acf201d18a692575324bc1ea
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-08-29 17:55:33 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -26,3 +26,16 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        return request.user and request.user.is_staff
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
