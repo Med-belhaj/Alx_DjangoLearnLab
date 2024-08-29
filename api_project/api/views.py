adb4f23bcdaa9b41920a05036105c86d97fa91c7
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-08-29 17:54:55 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -16,3 +16,13 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
