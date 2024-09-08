9e93b4d9ae02bf22049e09318cf6b230b85d1e8b
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-09-08 18:35:15 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -158,3 +158,93 @@ class BookListView(generics.ListAPIView):
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']  # Default ordering
+
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Book, Author
+
+class BookAPITests(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create test user
+        cls.user = User.objects.create_user(username='testuser', password='testpass')
+        
+        # Create test author and books
+        cls.author = Author.objects.create(name='Test Author')
+        cls.book = Book.objects.create(title='Test Book', publication_year=2024, author=cls.author)
+        cls.book_data = {
+            'title': 'New Book',
+            'publication_year': 2023,
+            'author': cls.author.id
+        }
+        cls.book_update_data = {
+            'title': 'Updated Book',
+            'publication_year': 2025,
+            'author': cls.author.id
+        }
+
+    def test_list_books(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Test Book')
+
+    def test_create_book_authenticated(self):
+        self.client.login(username='testuser', password='testpass')
+        url = reverse('book-list')
+        response = self.client.post(url, self.book_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+        self.assertEqual(Book.objects.latest('id').title, 'New Book')
+
+    def test_create_book_unauthenticated(self):
+        url = reverse('book-list')
+        response = self.client.post(url, self.book_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Book.objects.count(), 1)
+
+    def test_update_book_authenticated(self):
+        self.client.login(username='testuser', password='testpass')
+        url = reverse('book-detail', args=[self.book.id])
+        response = self.client.put(url, self.book_update_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, 'Updated Book')
+
+    def test_delete_book_authenticated(self):
+        self.client.login(username='testuser', password='testpass')
+        url = reverse('book-detail', args=[self.book.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_filter_books(self):
+        url = reverse('book-list') + '?title=Test Book'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Test Book')
+
+    def test_search_books(self):
+        url = reverse('book-list') + '?search=Test'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Test Book')
+
+    def test_order_books(self):
+        url = reverse('book-list') + '?ordering=title'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Test Book')
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        # Clean up any additional resources here if necessary
