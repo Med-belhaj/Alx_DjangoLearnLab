dfa588038af856041872f03be2890ff05bb3a159
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-09-08 18:20:31 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
+
 
 from rest_framework import generics
 from .models import Book
