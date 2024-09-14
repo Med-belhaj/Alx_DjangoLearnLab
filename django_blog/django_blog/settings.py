5e1c506644a9fe840e000c56756001d654f1acfb
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-09-14 16:51:01 +0100

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -143,3 +143,17 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Base directory path
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Directory to collect static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# Static root (where static files will be collected when running `collectstatic`)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
