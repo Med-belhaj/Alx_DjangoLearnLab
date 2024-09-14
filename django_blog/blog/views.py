f7467a2218cc3d8f124dd6e7e62db1d068ed7dfc
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-09-14 17:34:06 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,25 +1,41 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm  # Custom form
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-
+# Registration View
 def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login after successful registration
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
     else:
-        form = CustomUserCreationForm()
+        form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
+# Login View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
 
+# Logout View
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')
+
+# Profile View
 @login_required
 def profile(request):
-    return render(request, 'registration/profile.html')
+    return render(request, 'profile/profile.html')
