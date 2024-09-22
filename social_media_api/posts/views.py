e86a3c33e59cb5c58705e40e5379139c462144a9
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-09-22 22:33:02 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -33,3 +33,18 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Get posts from users the current user is following
+        followed_users = user.following.all()
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
