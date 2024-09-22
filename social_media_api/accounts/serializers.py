afbf0e1ca2c07b1ed4886ca51ac7c865d3d259e4
Med-belhaj <simohamed.belhaj2@gmail.com>
2024-09-22 17:57:07 +0100

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token  
+from rest_framework.authtoken.models import Token  # Import Token model for token creation
 from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
+# Get the user model
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -11,10 +12,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
-    password = serializers.CharField(
+    password = serializers.CharField(  # Using CharField for password
         write_only=True, required=True, validators=[validate_password]
     )
-    password2 = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)  # Second CharField for password confirmation
 
     class Meta:
         model = User
@@ -25,19 +26,22 @@ class RegisterSerializer(serializers.ModelSerializer):
         }
 
     def validate(self, attrs):
+        # Check if both password fields match
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(  # Using create_user for proper password handling
+        # Create user using create_user to handle password hashing
+        user = get_user_model().objects.create_user(  # Using create_user from get_user_model()
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             password=validated_data['password']  # No need to set_password separately
         )
-        Token.objects.create(user=user)  # Create a token for the user upon registration
+        # Create a token for the user upon registration
+        Token.objects.create(user=user)
         return user
 
 class UserSerializer(serializers.ModelSerializer):
